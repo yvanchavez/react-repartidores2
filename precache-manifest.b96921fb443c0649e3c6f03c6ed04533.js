bc9b8d2421442f8a756855a3f1abf2cf418184aa
Yvan Chavez <chavez.yvan@gmail.com>
2020-06-08 18:37:46 -0500

    crud

---
+++ precache-manifest.b96921fb443c0649e3c6f03c6ed04533.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40fe15e2dbf625d91771419920b4cdf4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "629634e4c26cca034074",
+    "url": "/static/js/2.a8fc794e.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.a8fc794e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ef9938c55187df61673",
+    "url": "/static/js/main.50ec8992.chunk.js"
+  },
+  {
+    "revision": "ef03fad86a919ef26f97",
+    "url": "/static/js/runtime-main.3ada473c.js"
+  }
+]);
